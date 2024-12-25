b6d918df807db5c9d103935bd0eee2de2041d631
aimee rivers <aimee@aimeerivers.com>
2024-12-25 15:41:13 +0100

    Initial Commit

---
+++ NextTrain/NextTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextTrainApp.swift
+//  NextTrain
+//
+//  Created by aimee rivers on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct NextTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NextTrainTests/NextTrainTests.swift
@@ -0,0 +1,17 @@
+//
+//  NextTrainTests.swift
+//  NextTrainTests
+//
+//  Created by aimee rivers on 25/12/2024.
+//
+
+import Testing
+@testable import NextTrain
+
+struct NextTrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
