d19a2f03e96dc15319acdb3a413703bb7d426afe
aimee rivers <aimee@aimeerivers.com>
2024-12-26 12:11:51 +0100

    chore: Add some more stations

--- NextTrain/Stations.swift
+++ NextTrain/Stations.swift
@@ -12,12 +12,19 @@ let allStations: [Station] = [
         id: "ALB", name: "Albertslund", latitude: 55.658056,
         longitude: 12.353889),
     Station(
+        id: "FS", name: "Frederikssund", latitude: 55.835833,
+        longitude: 12.065556
+    ),
+    Station(
+        id: "KL", name: "Klampenborg", latitude: 55.776667, longitude: 12.588333
+    ),
+    Station(
+        id: "KH", name: "København H", latitude: 55.672778, longitude: 12.564444
+    ),
+    Station(
         id: "MPT", name: "Malmparken", latitude: 55.724583, longitude: 12.386389
     ),
     Station(
         id: "SKO", name: "Skovlunde", latitude: 55.723, longitude: 12.403056),
-    Station(
-        id: "KH", name: "København H", latitude: 55.672778, longitude: 12.564444
-    ),
     // Add the rest of your stations here
 ]
