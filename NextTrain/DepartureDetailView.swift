0f56e7b8469270072ddae1cf73b6f12c6b5206b4
aimee rivers <aimee@aimeerivers.com>
2024-12-30 15:22:12 +0100

    fix: Show the sheet data as soon as it is available

--- NextTrain/DepartureDetailView.swift
+++ NextTrain/DepartureDetailView.swift
@@ -63,7 +63,7 @@ struct DepartureDetailView: View {
         .navigationTitle("Departure Details")
         .navigationBarTitleDisplayMode(.inline)
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            DispatchQueue.main.asyncAfter(deadline: .now()) {
                 isLoading = false
             }
         }
